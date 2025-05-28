84db1bdb48562e2a6eaa9eb471e04bdedc36e0fc
jcanett1 <jcanett@pxg.com>
2025-05-28 13:10:57 -0700

    Update apppruebabolsas.py

--- apppruebabolsas.py
+++ apppruebabolsas.py
@@ -563,7 +563,6 @@ def insert_divider_page(doc, label):
 # --- NUEVAS FUNCIONES PARA CLASIFICAR Y GENERAR PDFs POR CATEGORÍA ---
 
 def classify_item(item_code, item_description):
-    """Clasifica un ítem en 'Pelotas', 'Gorras', 'Guantes' o 'Accesorios'."""
     item_code_upper = item_code.upper()
     item_description_upper = item_description.upper()
 
@@ -571,13 +570,12 @@ def classify_item(item_code, item_description):
         return "Pelotas"
     elif item_code_upper.startswith('H-') or ("HAT" in item_description_upper or "CAP" in item_description_upper):
         return "Gorras"
-    # Clasificar guantes específicamente
     elif item_code_upper.startswith('G4-'):
         return "Guantes"
-    # Accesorios - Si no es pelota ni gorra ni guante, y empieza con A- o HC-
     elif item_code_upper.startswith(('A-', 'HC-')):
         return "Accesorios"
-    return "Otros"  # Para ítems que no encajan en ninguna categoría definida
+    return "Otros"
+
 
 def create_category_table(relations, category_name):
     """
