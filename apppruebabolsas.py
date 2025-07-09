3a267056684703ba6e94dcc4ac0788d443b43edc
jcanett1 <jcanett@pxg.com>
2025-07-09 09:38:01 -0700

    Update apppruebabolsas.py

--- apppruebabolsas.py
+++ apppruebabolsas.py
@@ -233,6 +233,7 @@ PART_DESCRIPTIONS = {
     'B-PG-246-POLY': 'Minimalist Carry Stand Bag - Black',
     'B-UGB8-EP': '2020 Carry Stand Bag - Black',
     'B-UGB14-FM': 'PXG Sunday Stand Bag - Black/White',
+    'B-UGB13-EP-B': 'Den Caddy- Black', 
 
     # --- CÓDIGOS DE LAS IMÁGENES ---
    # image_fbd711.png (Golf Balls)
